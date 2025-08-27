64e0dadf66649ea6a892089bd743204431aa1645
Alex <alesao19861986@gmail.com>
2025-08-27 20:44:56 +0300

    added libraries atexit

---
+++ a/atexit/exemple.py
@@ -0,0 +1,12 @@
+def goodbye(name, adjective):
+    print('Goodbye %s, it was %s to meet you.' % (name, adjective))
+
+import atexit
+
+atexit.register(goodbye, 'Donny', 'nice')
+print('66666666666')
+
+@atexit.register
+def goodbye():
+    print('You are now leaving the Python sector.')
+
