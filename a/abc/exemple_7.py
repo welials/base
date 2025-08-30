7af971f1e2ec238c78efb392f7e8389c5ca6a1e2
Alex <alesao19861986@gmail.com>
2025-08-30 10:58:05 +0300

    added libraries c

--- a/abc/exemple_7.py
+++ a/abc/exemple_7.py
@@ -7,3 +7,30 @@ class C(abc.ABC):
     @abc.abstractmethod
     def my_abstract_classmethod(cls, arg):
         ...
+
+class D(abc.ABC):
+    @staticmethod
+    @abc.abstractmethod
+    def my_abstract_staticmethod(arg):
+        ...
+
+class E(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def my_abstract_property(self):
+        ...
+
+class F(abc.ABC):
+    @property
+    def x(self):
+        ...
+
+    @x.setter
+    @abc.abstractmethod
+    def x(self, val):
+        ...
+
+class G(F):
+    @F.x.setter
+    def x(self, val):
+        ...
