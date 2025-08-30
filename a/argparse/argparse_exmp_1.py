64bd5d1ac651df8a9e6bce7c903bb0e0aee8d111
Alex <alesao19861986@gmail.com>
2025-08-30 14:18:13 +0300

    added libraries c

---
+++ a/argparse/argparse_exmp_1.py
@@ -0,0 +1,19 @@
+import argparse
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+parser.add_argument('filename')           # positional argument
+parser.add_argument('-c', '--count')      # option that takes a value
+parser.add_argument('-v', '--verbose',
+                    action='store_true')  # on/off flag
+
+args = parser.parse_args()
+print(args.filename, args.count, args.verbose)
+
+"""
+usage: ProgramName [-h] [-c COUNT] [-v] filename
+ProgramName: error: the following arguments are required: filename
+"""
+
---
+++ a/argparse/run_code.py
@@ -0,0 +1,23 @@
+import os
+
+
+if __name__ == '__main__':
+    os.system("python argparse_exmp_1.py -h")
+
+"""
+usage: ProgramName [-h] [-c COUNT] [-v] filename
+
+What the program does
+
+positional arguments:
+  filename
+
+options:
+  -h, --help         show this help message and exit
+  -c, --count COUNT
+  -v, --verbose
+
+Text at the bottom of help
+
+Process finished with exit code 0
+"""
