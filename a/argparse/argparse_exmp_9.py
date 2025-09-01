d159909a9fd68aec63374e67553b1c1581684cd0
Alex <alesao19861986@gmail.com>
2025-09-01 19:07:34 +0300

    added libraries argparse

--- a/argparse/argparse_exmp_9.py
+++ a/argparse/argparse_exmp_9.py
@@ -1,1 +1,13 @@
 import argparse
+
+"""
+class argparse.BooleanOptionalAction
+A subclass of Action for handling boolean flags with positive and negative options. 
+Adding a single argument such as --foo automatically creates both --foo and --no-foo options, storing True and False respectively:
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--foo', action=argparse.BooleanOptionalAction)
+print(parser.parse_args(['--no-foo']))
+"""
+Namespace(foo=False)
+"""
